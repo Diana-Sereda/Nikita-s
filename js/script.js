f2742914a21b5a6c328c1284d608008067080283
Diana-Sereda <69309199+Diana-Sereda@users.noreply.github.com>
2021-10-10 12:46:29 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,32 +8,69 @@ const cart=document.querySelector('.cart');
 const icon=document.querySelector('.search-icon');
 const search=document.querySelector('.search');
 const toClose=document.querySelector('.close');
+const closeTetxt=document.querySelector('.close-text');
 const searchInput=document.querySelector('.search-input');
 const invisic=document.querySelector('.invisic');
 const searchValue=document.getElementById('this-search');
 const result=document.querySelector('.search-result');
+const resultMobile=document.querySelector('.search-result-mobile');
+
 
 icon.onclick=function(){
-  search.classList.add('searching');
-  toClose.classList.remove('invisic');
-  searchInput.classList.remove('invisic');
-  cart.classList.toggle('invisic');
-  cover.style.display = "block";
-  result.style.display = "block";
-  body.style.overflow = "hidden";
-
-
+  if($(window).width() <= 834){
+    icon.classList.add('invisic');
+    closeTetxt.classList.remove('invisic');
+search.classList.add('searching');
+toClose.classList.remove('invisic');
+cart.classList.toggle('invisic');
+// cover.style.display = "block";
+resultMobile.style.display = "block";
+body.style.overflow = "hidden";
+  }else{
+search.classList.add('searching');
+toClose.classList.remove('invisic');
+searchInput.classList.remove('invisic');
+cart.classList.toggle('invisic');
+cover.style.display = "block";
+result.style.display = "block";
+body.style.overflow = "hidden";
+  }
 }
+
 toClose.onclick=function(){
  searchValue.value='';
  search.classList.remove('searching');
  toClose.classList.add('invisic');
  searchInput.classList.add('invisic');
   cart.classList.toggle('invisic');
+  resultMobile.style.display = "none";
   cover.style.display = "none";
   result.style.display = "none";
     body.style.overflow = "auto";
+    icon.classList.remove('invisic');
+    closeTetxt.classList.add('invisic');
 }
+// icon.onclick=function(){
+//   search.classList.add('searching');
+//   toClose.classList.remove('invisic');
+//   // closeTetxt.classList.remove('invisic');
+//   searchInput.classList.remove('invisic');
+//   cart.classList.toggle('invisic');
+//   cover.style.display = "block";
+//   result.style.display = "block";
+//   body.style.overflow = "hidden";
+// }
+// toClose.onclick=function(){
+//  searchValue.value='';
+//  search.classList.remove('searching');
+//  toClose.classList.add('invisic');
+// //  closeTetxt.classList.add('invisic');
+//  searchInput.classList.add('invisic');
+//   cart.classList.toggle('invisic');
+//   cover.style.display = "none";
+//   result.style.display = "none";
+//     body.style.overflow = "auto";
+// }
 
 
 //Show Cover 
@@ -64,3 +101,4 @@ summary.onclick=function(){
   document.getElementById('rus-img').src='img/icons/RU.svg';
   document.getElementById('en-img').src='img/icons/EN.svg';
 }
+
